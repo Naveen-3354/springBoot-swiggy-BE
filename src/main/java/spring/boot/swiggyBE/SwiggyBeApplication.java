b56fb47369572b75960f55783b0a6b8aa24b0fe5
Naveen-3354 <welbe1709@gmail.com>
2024-04-12 21:17:56 +0530

    first commit

---
+++ src/main/java/spring/boot/swiggyBE/SwiggyBeApplication.java
@@ -0,0 +1,14 @@
+package spring.boot.swiggyBE;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SwiggyBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SwiggyBeApplication.class, args);
+	}
+
+}
+// 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
